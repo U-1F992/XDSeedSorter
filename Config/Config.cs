1c2f73fd9896f8878aebaf1a3eaeba13bc28d68c
Kôtaro Mukai <mukai.k1011k@outlook.jp>
2022-04-22 22:59:49 +0900

    isolate config

---
+++ Config/Config.cs
@@ -0,0 +1,65 @@
+﻿using Sunameri;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// config.json
+/// </summary>
+public class Config
+{
+#pragma warning disable CS8618
+    [JsonPropertyName("portName")]
+    public string PortName { get; set; }
+    [JsonPropertyName("captureIndex")]
+    public int CaptureIndex { get; set; }
+    [JsonPropertyName("token")]
+    public string Token { get; set; }
+    [JsonPropertyName("tsv")]
+    public UInt32 Tsv { get; set; }
+    [JsonPropertyName("targets")]
+    public UInt32[] Targets { get; set; }
+    [JsonPropertyName("waitTime")]
+    public Dictionary<string, TimeSpan> WaitTime { get; set; }
+    [JsonPropertyName("advancesPerSecond")]
+    public double AdvancesPerSecond { get; set; }
+    [JsonPropertyName("allowLoad")]
+    public bool AllowLoad { get; set; }
+    [JsonPropertyName("advancesByLoading")]
+    public int AdvancesByLoading { get; set; }
+    [JsonPropertyName("advancesByOpeningItems")]
+    public int AdvancesByOpeningItems { get; set; }
+    [JsonPropertyName("sequences")]
+    public Dictionary<string, Operation[]> Sequences { get; set; }
+#pragma warning restore CS8618
+}
+/// <summary>
+/// <see cref="Config.Sequences"/> のKey一覧
+/// </summary>
+public static class Sequences
+{
+    public static readonly string Reset = "reset";
+    public static readonly string MoveQuickBattle = "moveQuickBattle";
+    public static readonly string LoadParties = "loadParties";
+    public static readonly string DiscardParties = "discardParties";
+    public static readonly string EntryToBattle = "entryToBattle";
+    public static readonly string ExitBattle = "exitBattle";
+    public static readonly string MoveMenu = "moveMenu";
+    public static readonly string MoveOptions = "moveOptions";
+    public static readonly string EnableVibration = "enableVibration";
+    public static readonly string DisableVibration = "disableVibration";
+    public static readonly string MoveContinue = "moveContinue";
+    public static readonly string Load = "load";
+    public static readonly string MoveSave = "moveSave";
+    public static readonly string Save = "save";
+    public static readonly string MoveItems = "moveItems";
+    public static readonly string OpenCloseItems = "openCloseItems";
+    public static readonly string WatchSteps = "watchSteps";
+    public static readonly string Finalize = "finalize";
+}
+/// <summary>
+/// <see cref="Config.WaitTime"/> のKey一覧
+/// </summary>
+public static class WaitTime
+{
+    public static readonly string Maximum = "maximum";
+    public static readonly string Left = "left";
+}
--- Sorter/Sorter.cs
+++ Sorter/Sorter.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json;
 using System.IO.Ports;
 
 using LINENotify;
@@ -461,71 +459,6 @@ public class Sorter : IDisposable
         for (var i = 0; i < advances.WatchSteps; i++)
             await _serialPort.RunAsync(_config.Sequences[Sequences.WatchSteps], cancellationToken);
     }
-
-    #region Config(config.json)
-    /// <summary>
-    /// config.json
-    /// </summary>
-    class Config
-    {
-#pragma warning disable CS8618
-        [JsonPropertyName("portName")]
-        public string PortName { get; set; }
-        [JsonPropertyName("captureIndex")]
-        public int CaptureIndex { get; set; }
-        [JsonPropertyName("token")]
-        public string Token { get; set; }
-        [JsonPropertyName("tsv")]
-        public UInt32 Tsv { get; set; }
-        [JsonPropertyName("targets")]
-        public UInt32[] Targets { get; set; }
-        [JsonPropertyName("waitTime")]
-        public Dictionary<string, TimeSpan> WaitTime { get; set; }
-        [JsonPropertyName("advancesPerSecond")]
-        public double AdvancesPerSecond { get; set; }
-        [JsonPropertyName("allowLoad")]
-        public bool AllowLoad { get; set; }
-        [JsonPropertyName("advancesByLoading")]
-        public int AdvancesByLoading { get; set; }
-        [JsonPropertyName("advancesByOpeningItems")]
-        public int AdvancesByOpeningItems { get; set; }
-        [JsonPropertyName("sequences")]
-        public Dictionary<string, Operation[]> Sequences { get; set; }
-#pragma warning restore CS8618
-    }
-    /// <summary>
-    /// <see cref="Config.Sequences"/> のKey一覧
-    /// </summary>
-    static class Sequences
-    {
-        public static readonly string Reset = "reset";
-        public static readonly string MoveQuickBattle = "moveQuickBattle";
-        public static readonly string LoadParties = "loadParties";
-        public static readonly string DiscardParties = "discardParties";
-        public static readonly string EntryToBattle = "entryToBattle";
-        public static readonly string ExitBattle = "exitBattle";
-        public static readonly string MoveMenu = "moveMenu";
-        public static readonly string MoveOptions = "moveOptions";
-        public static readonly string EnableVibration = "enableVibration";
-        public static readonly string DisableVibration = "disableVibration";
-        public static readonly string MoveContinue = "moveContinue";
-        public static readonly string Load = "load";
-        public static readonly string MoveSave = "moveSave";
-        public static readonly string Save = "save";
-        public static readonly string MoveItems = "moveItems";
-        public static readonly string OpenCloseItems = "openCloseItems";
-        public static readonly string WatchSteps = "watchSteps";
-        public static readonly string Finalize = "finalize";
-    }
-    /// <summary>
-    /// <see cref="Config.WaitTime"/> のKey一覧
-    /// </summary>
-    static class WaitTime
-    {
-        public static readonly string Maximum = "maximum";
-        public static readonly string Left = "left";
-    }
-    #endregion
 
     #region IDisposable implementation
     private bool disposedValue;
